05b1b808ac74c17e0e5e334712730de154429b13
Awa Masashi <kenshin561183@gmail.com>
2025-02-06 23:11:20 +0900

    [update] template.ini更新

--- bin/template-initial.sh
+++ bin/template-initial.sh
@@ -3,7 +3,7 @@
 set -eu
 script_dir=$(cd $(dirname $0);pwd)
 root_dir="${script_dir}/.."
-ini_file="${root_dir}/template.ini"
+ini_file="${script_dir}/template.ini"
 
 export LC_ALL=C
 
@@ -11,9 +11,9 @@ ini_keys=`cat $ini_file | grep "=" | sed -e "s/=.*//g"`
 cd ${root_dir}/
 for ini_key in ${ini_keys} ; do
   ini_value=`cat $ini_file | grep $ini_key | sed -e "s/.*=\(.*\)/\1/g"`
-  
+
   echo;
-  echo "____ Replace <${ini_key}> with ${ini_value}: start. ____"
+  echo "Replace <${ini_key}> with ${ini_value}: start."
   echo "Check if <${ini_key}> exists in the repository."
 
   grep_result=`find . -type f | grep -v ".git/" | xargs grep "<${ini_key}>" | wc -l | sed -e "s/ //g"`
@@ -24,8 +24,8 @@ for ini_key in ${ini_keys} ; do
   echo "Replace <${ini_key}> with ${ini_value}."
   else
     find . -type f | grep -v ".git/" | xargs sed -i "" "s/<${ini_key}>/${ini_value}/g"
-    echo "The replacment process scceeded!"
+    echo "The replacement process succeeded!"
   fi
 
-  echo "____ Replace <${ini_key}> with ${ini_value}: end. ____"
+  echo "Replace <${ini_key}> with ${ini_value}: end."
 done
