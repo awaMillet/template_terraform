11c5087afd2f0553f181585cfff0a2cfaf1f8229
Awa Masashi <kenshin561183@gmail.com>
2025-06-01 16:47:27 +0900

    [add] パブリックルートテーブル作成

--- modules/network/subnet.tf
+++ modules/network/subnet.tf
@@ -32,6 +32,38 @@ resource "aws_subnet" "public_d" {
   }
 }
 
+## インターネットゲートウェイへのルート
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.main.id
+  }
+
+  tags = {
+    Name = "${var.env_str}-public-route-table"
+  }
+
+  depends_on = [aws_internet_gateway.main]
+}
+
+## パブリックサブネットの関連付け
+resource "aws_route_table_association" "public_a" {
+  subnet_id      = aws_subnet.public_a.id
+  route_table_id = aws_route_table.public.id
+}
+
+resource "aws_route_table_association" "public_c" {
+  subnet_id      = aws_subnet.public_c.id
+  route_table_id = aws_route_table.public.id
+}
+
+resource "aws_route_table_association" "public_d" {
+  subnet_id      = aws_subnet.public_d.id
+  route_table_id = aws_route_table.public.id
+}
+
 # private subnet
 resource "aws_subnet" "private_a" {
   vpc_id            = aws_vpc.main.id
