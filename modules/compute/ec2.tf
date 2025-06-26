78984594bb1fa8cce668de414d139ef178852b4e
Awa Masashi <kenshin561183@gmail.com>
2025-06-27 00:14:36 +0900

    [update] ec2

--- modules/compute/ec2.tf
+++ modules/compute/ec2.tf
@@ -1,10 +1,23 @@
-resource "aws_instance" "NAME" {
-  instance_type = var.target_instance_type
-  ami           = var.target_ami
-  subnet_id     = var.target_subnet
-  # iam_instance_profile = aws_iam_role.INSTANCEPROFILE.id
+resource "aws_instance" "ec2" {
+  instance_type                        = var.target_instance_type
+  ami                                  = var.target_ami
+  subnet_id                            = var.target_subnet
+  availability_zone                    = var.target_az
+  iam_instance_profile                 = aws_iam_instance_profile.ec2.id
+  key_name                             = ""
+  hibernation                          = false
+  instance_initiated_shutdown_behavior = "stop"
+  vpc_security_group_ids               = [aws_security_group.ec2.id]
 
-  tags = {
-    Name = var.tag_name
+  root_block_device {
+    volume_type           = "gp3"
+    volume_size           = 100
+    iops                  = 3000
+    delete_on_termination = true
+    encrypted             = false
+
+    tags = {
+      Name = var.tag_name
+    }
   }
 }
--- src/dev/ec2.tf
+++ src/dev/ec2.tf
@@ -1,6 +1,8 @@
 module "ec2" {
   source = "../../modules/compute"
 
+  env_str       = "dev"
+  vpc_id_str    = module.network.vpc_id
   target_subnet = module.network.private_a_id
   tag_name      = "sample"
 }
